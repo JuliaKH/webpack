3ecc835888e4c17a062296b571efce8a02083268
JuliaKH <43015141+JuliaKH@users.noreply.github.com>
2019-07-06 20:29:57 +0300

    added webpack, babel

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            },
+            exclude: '/node_modules/'
+        }]
+    },
+    devServer:{
+        overlay: true
+    }
+};
